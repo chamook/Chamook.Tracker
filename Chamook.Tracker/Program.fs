377ec5090729d089c146d7dd50ac7d073a13795b
Adam Guest <adam.guest@247e.com>
2015-04-25 08:50:59 +0200

    Tracking different conditions relative to a home point and sending notifications based on some states

--- Chamook.Tracker/Program.fs
+++ Chamook.Tracker/Program.fs
@@ -3,6 +3,9 @@ open System
 open System.Threading
 open Newtonsoft.Json
 
+//todo: load this from a config file or something
+let home = Haversine.pos(0.0<Haversine.deg>, 0.0<Haversine.deg>)
+
 let login username pin = 
     let data = "username=" + username + "&pin=" + pin + "&image.x=0&image.y=0"
     createRequest Post "http://79.123.36.5/mmlogin2.asp"
@@ -29,36 +32,74 @@ let filterTrackingResponse (response:string) =
     let endPoint = response.IndexOf(",streetViewControl")
     response.Substring(startPoint, endPoint - startPoint) + "}"
 
-let loginPrompt = 
+let loginPrompt ()= 
     Console.WriteLine "Enter Username:"
     let user = Console.ReadLine()
     Console.WriteLine "Enter PIN:"
     let pin = Console.ReadLine()
     user, pin
 
-let home = Haversine.pos(0.0<Haversine.deg>, 0.0<Haversine.deg>)
+type historicalPosition = {position:Haversine.pos;distanceFromHome:float<Haversine.km>}
+
+type positionAnalysis = 
+    |AwayFromHomeFirstTime
+    |AwayFromHomeMoving
+    |AwayFromHomeStationary
+    |AtHome
+
+let comparePositions previousPosition currentPosition = 
+    if currentPosition.distanceFromHome < 0.4<Haversine.km> then
+        AtHome
+    else
+        if previousPosition.distanceFromHome < 0.4<Haversine.km> then
+            AwayFromHomeFirstTime
+        else
+            if currentPosition.distanceFromHome - previousPosition.distanceFromHome < 0.1<Haversine.km> then
+                AwayFromHomeStationary
+            else
+                AwayFromHomeMoving
+    
+
+let processPosition (pos:Haversine.pos, history:list<historicalPosition>) = 
+    let distance = pos |> Haversine.hsDist home
+    let newPosition = {position=pos;distanceFromHome=distance}
+    match history with
+    | [] -> [newPosition]
+    | head::_ ->    match comparePositions head newPosition with
+                    |AtHome -> newPosition::history
+                    |AwayFromHomeMoving -> newPosition::history
+                    |AwayFromHomeFirstTime ->   Pushbullet.sendPush "chamookdk@gmail.com" "Movement Alert" "Away from home first time"
+                                                newPosition::history
+                    |AwayFromHomeStationary ->  Pushbullet.sendPush "chamookdk@gmail.com" "Movement Alert" "Away from home stationary"
+                                                newPosition::history
+
+                     
+    
 
 
-let rec track user password = 
+let rec track (history:list<historicalPosition>, user:string, password:string) = 
     let loginResponse =  login user password
     let trackingResponse = loginResponse.Cookies.["ASPSESSIONIDACSCBRTS"] |> openTrackingPage
 
-    match trackingResponse.EntityBody with
-    |Some s ->  let distanceFromHome = filterTrackingResponse s |> parsePosition |> Haversine.hsDist home
-                if distanceFromHome>0.4<Haversine.km> then
-                    Console.WriteLine "AWAY FROM HOME"
-                else
-                    Console.WriteLine "at home"
-    |None -> Console.WriteLine "No data!"
+    let newHistory =    match trackingResponse.EntityBody with
+                        |Some s -> 
+                            let parsedPosition = filterTrackingResponse s |> parsePosition
+                            processPosition(parsedPosition,history)
+                        |None -> history
 
     Thread.Sleep 240000
      
-    track user password
-    
+    track(newHistory,user,password)
 
 [<EntryPoint>]
 let main argv = 
-    loginPrompt ||> track
+    //there has to be a less dumb way to do this
+    let userData = loginPrompt()
+    let username = fst userData
+    let pwd = snd userData
+
+    track([],username,pwd) |> ignore
+
 
     Console.ReadLine |> ignore
     0
